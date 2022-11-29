9a38f91ae45b8d712a4a7ca4247db3c4e30b6475
StarkDroid <gamerprince.exp@gmail.com>
2022-11-29 15:48:29 +0800

    Launcher3: Add toggle to show/hide app drawer search bar
    Change-Id: I66bb9a232e7e49a94a1c8b11bea195a5d8faa6d1
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -178,6 +178,7 @@ public final class Utilities {
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
     public static final String KEY_STATUS_BAR = "pref_show_statusbar";
     public static final String KEY_FORCE_MONOCHROME_ICONS = "pref_forced_monochrome_icons";
+    public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
 
     /**
      * Returns true if theme is dark.
@@ -936,5 +937,10 @@ public final class Utilities {
     public static boolean enableMonoChromeThemedIcons(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_FORCE_MONOCHROME_ICONS, false);
+    }
+
+    public static boolean showSearch(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_DRAWER_SEARCH, true);
     }
 }
